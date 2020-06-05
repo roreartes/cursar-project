be7994cab29a51100bd16a79a58705f428ab1b76
Romina <rominar2008@gmail.com>
2020-06-05 17:53:38 -0300

    fix error in method

--- src/main/java/ar/com/ada/cursarproject/cursarproject/model/repository/RepresentantRepository.java
+++ src/main/java/ar/com/ada/cursarproject/cursarproject/model/repository/RepresentantRepository.java
@@ -4,7 +4,7 @@ import ar.com.ada.cursarproject.cursarproject.model.entity.Representant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
-@Service("representantService")
+@Service("representantRepository")
 public interface RepresentantRepository extends JpaRepository<Representant, Long> {
 
 }
