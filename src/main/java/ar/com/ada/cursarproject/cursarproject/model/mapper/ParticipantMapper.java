102e18a33996430a7d5f2e77f22f5c6298b66897
Romina <rominar2008@gmail.com>
2020-06-16 10:16:24 -0300

    fix error in class mapper

--- src/main/java/ar/com/ada/cursarproject/cursarproject/model/mapper/ParticipantMapper.java
+++ src/main/java/ar/com/ada/cursarproject/cursarproject/model/mapper/ParticipantMapper.java
@@ -2,8 +2,10 @@ package ar.com.ada.cursarproject.cursarproject.model.mapper;
 
 import ar.com.ada.cursarproject.cursarproject.model.dto.ParticipantDTO;
 import ar.com.ada.cursarproject.cursarproject.model.entity.Participant;
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+@Mapper(componentModel = "spring")
 public interface ParticipantMapper extends DataCyclerMapper<ParticipantDTO, Participant> {
     ParticipantMapper MAPPER = Mappers.getMapper(ParticipantMapper.class);
 
