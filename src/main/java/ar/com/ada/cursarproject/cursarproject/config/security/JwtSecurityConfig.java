83b437eebbd833805ab2981cbfb6ca5937f1e622
Romina <rominar2008@gmail.com>
2020-06-29 10:47:03 -0300

    configuracion paquete config

--- src/main/java/ar/com/ada/cursarproject/cursarproject/config/security/JwtSecurityConfig.java
+++ src/main/java/ar/com/ada/cursarproject/cursarproject/config/security/JwtSecurityConfig.java
@@ -1,5 +1,6 @@
 package ar.com.ada.cursarproject.cursarproject.config.security;
 
+
 import ar.com.ada.cursarproject.cursarproject.component.security.RestAuthEntryPoint;
 import ar.com.ada.cursarproject.cursarproject.services.security.JwtUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,15 +8,23 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class JwtSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired @Qualifier("jwtUserDetailsService")
+    @Autowired
+    @Qualifier("jwtUserDetailsService")
     private JwtUserDetailsService jwtUserDetailsService;
 
     @Autowired @Qualifier("restAuthEntryPoint")
--- src/main/java/ar/com/ada/cursarproject/cursarproject/services/security/JwtUserDetailsService.java
+++ src/main/java/ar/com/ada/cursarproject/cursarproject/services/security/JwtUserDetailsService.java
@@ -3,11 +3,12 @@ package ar.com.ada.cursarproject.cursarproject.services.security;
 
 
 import ar.com.ada.cursarproject.cursarproject.component.security.JwtAuthProvider;
+import ar.com.ada.cursarproject.cursarproject.model.dto.security.JwtUserDetails;
 import ar.com.ada.cursarproject.cursarproject.model.repository.security.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -23,7 +24,7 @@ public class JwtUserDetailsService implements UserDetailsService {
     private JwtAuthProvider jwtAuthProvider;
 
     @Override @Transactional
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public JwtUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User Not Found: " + username));
 
